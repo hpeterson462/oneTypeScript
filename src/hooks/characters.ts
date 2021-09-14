a09360e52fcb878e20df654f3305705eb3836da9
Heather Peterson <hpeterson462@gmail.com>
2021-09-14 15:56:19 -0700

    single character hook

--- src/hooks/characters.ts
+++ src/hooks/characters.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { getCharacters } from '../services/character-apis';
+import { useParams } from 'react-router';
+import { getCharacters, getCharactersById } from '../services/character-apis';
 import { ListCharacter } from '../types/types';
 
 export const useCharacters = () => {
@@ -15,5 +16,23 @@ export const useCharacters = () => {
   return {
     loading,
     characters
+  };
+};
+
+export const useSingleCharacter = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [character, setCharacter] = useState<Character>();
+
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    getCharactersById(id)
+      .then((fetchedCharacter) => setCharacter(fetchedCharacter))
+      .then(() => setLoading(false));
+  }, [id]);
+
+  return {
+    loading,
+    character
   };
 };
